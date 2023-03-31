dbb04224d4313f390e27522d44232bb1fd8cfb21
Pingvu <nhlpool27@gmail.com>
2023-03-31 09:08:27 +0200

    Add project files.

---
+++ inlämningsuppgifter 5.1 array/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] meningar = new string[4];
+            meningar[0] = "veckorna är långa, helgerna är korta.";
+            meningar[1] = "gräs är grönt";
+            meningar[2] = "hamnburgare är inte dålig mat";
+            meningar[3] = "det är snart helg";
+            int x = 0;
+            do
+            {
+                Console.WriteLine(meningar[x]);
+                Console.WriteLine(" ");
+                x++;
+            }
+            while (x<4);
+        }
+    }
+}
